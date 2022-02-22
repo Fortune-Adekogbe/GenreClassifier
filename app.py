b68ee5e1e70206da139348d10da7cface884d05d
fortune-adekogbe <fortuneadekogbe@gmail.com>
2022-02-22 14:42:28 +0100

    edited app file.

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import subprocess, platform
-
-if platform.system() == 'Linux':
-    subprocess.run(['apt-get', 'install', 'libsndfile1'], check=True)
-    subprocess.run(['apt-get', 'install', 'ffmpeg'], check=True)
-
 import gradio as gr
 from inference import *
 
